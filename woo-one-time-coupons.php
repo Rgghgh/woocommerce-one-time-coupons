59ffc4546ae400389b2b0940914df0f24c66b89f
Rgghgh <tomer1091@gmail.com>
2020-09-28 17:01:19 +0300

    review fixes

--- woo-one-time-coupons.php
+++ woo-one-time-coupons.php
@@ -10,10 +10,10 @@ Text Domain: woo-one-time-coupons
 Domain Path: /languages
 */
 
-define("WC_OTC_VERSION", '1.0.1');
-define("WC_OTC_TEXT_DOMAIN", 'woo-one-time-coupons');
-define("WC_OTC_TABLE", "wc_one_time_coupons");
-define("WC_OTC_COUPON_LENGTH", 8);
+define("WCOTC_VERSION", '1.0.1');
+define("WCOTC_TEXT_DOMAIN", 'woo-one-time-coupons');
+define("WCOTC_TABLE", "wc_one_time_coupons");
+define("WCOTC_COUPON_LENGTH", 8);
 
 /**
  * Setup
@@ -22,7 +22,7 @@ define("WC_OTC_COUPON_LENGTH", 8);
 register_activation_hook(__FILE__, function () {
     global $wpdb;
     $charset_collate = $wpdb->get_charset_collate();
-    $table_name = $wpdb->prefix . WC_OTC_TABLE;
+    $table_name = $wpdb->prefix . WCOTC_TABLE;
 
     $sql = "CREATE TABLE $table_name (
 			ID BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT, 
@@ -36,7 +36,7 @@ register_activation_hook(__FILE__, function () {
     require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
     dbDelta($sql);
 
-    update_option('wc_otc_version', WC_OTC_VERSION);
+    update_option('wcotc_version', WCOTC_VERSION);
 });
 
 /**
@@ -45,7 +45,7 @@ register_activation_hook(__FILE__, function () {
 
 add_filter('woocommerce_coupon_data_tabs', function ($tabs) {
     $tabs['wc_otc_coupon_data'] = array(
-        'label' => __('Coupon Codes', WC_OTC_TEXT_DOMAIN),
+        'label' => __('Coupon Codes', WCOTC_TEXT_DOMAIN),
         'target' => 'wc_otc_coupon_data',
         'class' => 'wc_otc_coupon_data'
     );
@@ -58,14 +58,14 @@ add_action('woocommerce_coupon_data_panels', function ($coupon_id, $coupon) {
 
     woocommerce_wp_checkbox(array(
         'id' => 'is_otc_coupon',
-        'label' => __('Enable one time use codes', WC_OTC_TEXT_DOMAIN),
-        'description' => __('Allow this coupon to have multiple coupon codes', WC_OTC_TEXT_DOMAIN),
+        'label' => __('Enable one time use codes', WCOTC_TEXT_DOMAIN),
+        'description' => __('Allow this coupon to have multiple coupon codes', WCOTC_TEXT_DOMAIN),
     ));
 
     woocommerce_wp_text_input(array(
         'id' => 'otc_coupon_prefix',
-        'label' => __('Coupon Prefixes', WC_OTC_TEXT_DOMAIN),
-        'description' => __('Will be prepended to generated coupons', WC_OTC_TEXT_DOMAIN),
+        'label' => __('Coupon Prefixes', WCOTC_TEXT_DOMAIN),
+        'description' => __('Will be prepended to generated coupons', WCOTC_TEXT_DOMAIN),
         'type' => 'text',
         'desc_tip' => true,
         'class' => 'short'
@@ -73,8 +73,8 @@ add_action('woocommerce_coupon_data_panels', function ($coupon_id, $coupon) {
 
     woocommerce_wp_text_input(array(
         'id' => 'codes_to_generate',
-        'label' => __('Codes amount', WC_OTC_TEXT_DOMAIN),
-        'description' => __('The amount of one-time-use codes to generate', WC_OTC_TEXT_DOMAIN),
+        'label' => __('Codes amount', WCOTC_TEXT_DOMAIN),
+        'description' => __('The amount of one-time-use codes to generate', WCOTC_TEXT_DOMAIN),
         'type' => 'number',
         'desc_tip' => true,
         'class' => 'short'
@@ -85,30 +85,30 @@ add_action('woocommerce_coupon_data_panels', function ($coupon_id, $coupon) {
 
     woocommerce_wp_textarea_input(array(
         'id' => 'free_codes',
-        'label' => __('Free Codes', WC_OTC_TEXT_DOMAIN),
-        'value' => get_codes($coupon_id, WC_OTC_FREE),
+        'label' => __('Free Codes', WCOTC_TEXT_DOMAIN),
+        'value' => wcotc_get_codes($coupon_id, WCOTC_FREE),
     ));
 
     woocommerce_wp_textarea_input(array(
         'id' => 'used_codes',
-        'label' => __('Used Codes', WC_OTC_TEXT_DOMAIN),
-        'value' => get_codes($coupon_id, WC_OTC_USED),
+        'label' => __('Used Codes', WCOTC_TEXT_DOMAIN),
+        'value' => wcotc_get_codes($coupon_id, WCOTC_USED),
     ));
 
     echo '</div></div>';
 }, 10, 2);
 
-define('WC_OTC_ALL', 0);
-define('WC_OTC_FREE', 1);
-define('WC_OTC_USED', 2);
+define('WCOTC_ALL', 0);
+define('WCOTC_FREE', 1);
+define('WCOTC_USED', 2);
 
-function get_codes($coupon_id, $state = WC_OTC_ALL)
+function wcotc_get_codes($coupon_id, $state = WCOTC_ALL)
 {
     global $wpdb;
-    $table_name = $wpdb->prefix . WC_OTC_TABLE;
+    $table_name = $wpdb->prefix . WCOTC_TABLE;
     $sql = "SELECT code FROM $table_name WHERE coupon_id=%d";
-    if ($state == WC_OTC_FREE) $sql .= " AND order_id IS NULL";
-    if ($state == WC_OTC_USED) $sql .= " AND order_id IS NOT NULL";
+    if ($state == WCOTC_FREE) $sql .= " AND order_id IS NULL";
+    if ($state == WCOTC_USED) $sql .= " AND order_id IS NOT NULL";
 
     $result = $wpdb->get_results($wpdb->prepare($sql, $coupon_id));
 
@@ -120,15 +120,15 @@ function get_codes($coupon_id, $state = WC_OTC_ALL)
 }
 
 add_action('woocommerce_process_shop_coupon_meta', function ($post_id) {
-    update_post_meta($post_id, 'is_otc_coupon', $_POST['is_otc_coupon']);
+    update_post_meta($post_id, 'is_otc_coupon', boolval($_POST['is_otc_coupon']));
     update_post_meta($post_id, 'otc_coupon_prefix', wc_format_coupon_code(trim($_POST['otc_coupon_prefix'])));
-    if ($_POST['codes_to_generate'])
-        do_action('wc_otc_generate_codes', $post_id, (int)$_POST['codes_to_generate']);
+    if ($_POST['codes_to_generate'] && is_numeric($_POST['codes_to_generate']))
+        do_action('wcotc_generate_codes', $post_id, (int)$_POST['codes_to_generate']);
 }, 10, 1);
 
-add_action('wc_otc_generate_codes', function ($post_id, $count) {
+add_action('wcotc_generate_codes', function ($post_id, $count) {
     global $wpdb;
-    $table_name = $wpdb->prefix . WC_OTC_TABLE;
+    $table_name = $wpdb->prefix . WCOTC_TABLE;
     $prefix = get_post_meta($post_id, 'otc_coupon_prefix', true);
 
     $values = [];
@@ -136,7 +136,7 @@ add_action('wc_otc_generate_codes', function ($post_id, $count) {
     $query = "INSERT INTO $table_name (coupon_id, code) VALUES ";
 
     for ($i = 0; $i < $count; $i++) {
-        $code = $prefix . substr(wp_generate_uuid4(), 0, WC_OTC_COUPON_LENGTH);
+        $code = $prefix . substr(wp_generate_uuid4(), 0, WCOTC_COUPON_LENGTH);
         array_push($values, $post_id, $code);
         $place_holders[] = "('%d', '%s')";
     }
@@ -151,7 +151,7 @@ add_action('wc_otc_generate_codes', function ($post_id, $count) {
 
 add_filter('woocommerce_get_shop_coupon_data', function ($false, $code, WC_Coupon $wc_coupon) {
     global $wpdb;
-    $table_name = $wpdb->prefix . WC_OTC_TABLE;
+    $table_name = $wpdb->prefix . WCOTC_TABLE;
 
     $sql = "SELECT * FROM $table_name WHERE code='%s'";
     $otc_coupon = $wpdb->get_row($wpdb->prepare($sql, $code), ARRAY_A);
@@ -177,16 +177,16 @@ add_filter('woocommerce_coupon_is_valid', function ($value, WC_Coupon $coupon) {
     return $value;
 }, 1, 2);
 
-add_action('woocommerce_order_status_pending', 'wc_update_otc_coupon_usage');
-add_action('woocommerce_order_status_completed', 'wc_update_otc_coupon_usage');
-add_action('woocommerce_order_status_processing', 'wc_update_otc_coupon_usage');
-add_action('woocommerce_order_status_on-hold', 'wc_update_otc_coupon_usage');
-add_action('woocommerce_order_status_cancelled', 'wc_update_otc_coupon_usage');
+add_action('woocommerce_order_status_pending', 'wcotc_update_otc_coupon_usage');
+add_action('woocommerce_order_status_completed', 'wcotc_update_otc_coupon_usage');
+add_action('woocommerce_order_status_processing', 'wcotc_update_otc_coupon_usage');
+add_action('woocommerce_order_status_on-hold', 'wcotc_update_otc_coupon_usage');
+add_action('woocommerce_order_status_cancelled', 'wcotc_update_otc_coupon_usage');
 
-function wc_update_otc_coupon_usage($order_id)
+function wcotc_update_otc_coupon_usage($order_id)
 {
     global $wpdb;
-    $table_name = $wpdb->prefix . WC_OTC_TABLE;
+    $table_name = $wpdb->prefix . WCOTC_TABLE;
     $sql = "SELECT * FROM $table_name WHERE code='%s'";
 
     $order = wc_get_order($order_id);
